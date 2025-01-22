7c1203b404dee34a778e7f7172d624852d3e817e
apear-e <145722202+apear-e@users.noreply.github.com>
2025-01-22 09:05:19 -0700

    GitHub submission for ICE2

--- src/halfAdder.vhd
+++ src/halfAdder.vhd
@@ -9,12 +9,16 @@
 --| USAF Academy, CO 80840           \____//____/_/  |_/_/   /_/  |_|
 --| 
 --| ---------------------------------------------------------------------------
---|
+--| FILENAME      : halfAdder.vhd
+--| AUTHOR(S)     : Adam Pearse, Grant Stec
+--| CREATED       : 01/22/2025
 --| DESCRIPTION   : This file implements a one bit half adder.
+--|
+--| DOCUMENTATION : None
 --|
 --+----------------------------------------------------------------------------
 --|
---| NAMING CONVENSIONS :
+--| NAMING CONVENTIONS :
 --|
 --|    i_<port name>            = on-chip input port
 --|    o_<port name>            = on-chip output port
@@ -29,8 +33,8 @@ entity halfAdder is
   port(
 	i_A     : in  std_logic; -- 1-bit input port
 	i_B     : in  std_logic; 
-	o_S     : out std_logic  -- 1-bit output port (NOTE: NO semicolon on LAST port only!)	    
-	-- TODO:  Carry port
+	o_S     : out std_logic;  -- 1-bit output port for Sum signal (NOTE: NO semicolon on LAST port only!)	    
+	o_Cout  : out std_logic  --1-bit output port for Carry signal
   ); -- the semicolon is here instead
 end halfAdder;
 
@@ -43,5 +47,6 @@ begin
 	-- *concurrent* signal assignments
 	o_S    <= i_A xor i_B;
 	-- TODO:  Carry signal assignment
+	o_Cout <= i_A and i_B;
 	
 end halfAdder_arch;
